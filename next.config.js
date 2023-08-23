eb1642df7a350c0180437624445fddbebba0d6d4
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2023-08-23 11:31:08 -0300

    Initial

--- next.config.js
+++ next.config.js
@@ -1,37 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['gasos-ballot-gallery-resized.s3.us-east-1.amazonaws.com']
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Credentials',
-            value: String(process.env.ACCESS_CONTROL_ALLOW_CREDENTIALS)
-          },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: String(process.env.ACCESS_CONTROL_ALLOW_ORIGIN)
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: String(process.env.ACCESS_CONTROL_ALLOW_METHODS)
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: String(process.env.ACCESS_CONTROL_ALLOW_HEADERS)
-          }
-        ]
-      }
-    ];
-  }
-  // output: 'export',
-  // trailingSlash: true,
-  // images: { unoptimized: true }
+  // images: {
+  //   domains: ['gasos-ballot-gallery-resized.s3.us-east-1.amazonaws.com']
+  // },
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: '/api/:path*',
+  //       headers: [
+  //         {
+  //           key: 'Access-Control-Allow-Credentials',
+  //           value: String(process.env.ACCESS_CONTROL_ALLOW_CREDENTIALS)
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Origin',
+  //           value: String(process.env.ACCESS_CONTROL_ALLOW_ORIGIN)
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Methods',
+  //           value: String(process.env.ACCESS_CONTROL_ALLOW_METHODS)
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Headers',
+  //           value: String(process.env.ACCESS_CONTROL_ALLOW_HEADERS)
+  //         }
+  //       ]
+  //     }
+  //   ];
+  // }
 };
 
 module.exports = nextConfig;
