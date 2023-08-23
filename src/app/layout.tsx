40cd046fed1bf16627fafe926ef99b8fba781742
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2023-08-23 12:47:41 -0300

    Build fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // const currentuser = await getCurrentUser();
-  const currentuser = null;
+  const currentuser = await getCurrentUser();
 
   return (
     <html lang="en">
